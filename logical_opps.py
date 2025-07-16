da065b45341920f75a0b6f3200475f5a328b62fd
Yutory <bhareyutory@gmail.com>
2025-07-16 13:22:05 +0100

    logical_opps.py

--- logical_opps.py
+++ logical_opps.py
@@ -1,4 +1,52 @@
-first_n = 6
-second_n = 5
-print(first_n or second_n)
+# A simple calculator app
+
+print('''---------------------------------------------------------
+1. Addition
+2. Subtraction 
+3. Multiplication
+4. Division
+5. Exponential									-------------------------------------------------------------- ''')
+
+print("Enter two numbers to add:")
+#prompt a user for a number and save it
+first_n = input("first number:")
+#prompt a user for a number and save it
+second_n = input("second number:")
+sum =float(first_n) + float(second_n)
+print(f"{first_n} + {second_n} = {sum:.2f}")
+
+print("Enter two numbers to sub:")
+#prompt a user for a number and save it
+first_n = input("first number:")
+#prompt a user for a number and save it
+second_n = input("second number:")
+sum =float(first_n) - float(second_n)
+print(f"{first_n} - {second_n} = {sum:.2f}")
+
+#multiplication
+print("Enter two numbers to mul:")
+#prompt a user for a number and save it
+first_n = input("first number:")
+#prompt a user for a number and save it
+second_n = input("second number:")
+sum =float(first_n) * float(second_n)
+print(f"{first_n} * {second_n} = {sum}:.2f")
+
+#division
+print("Enter two numbers to divide:")
+#prompt a user for a number and save it
+first_n = input("first number:")
+#prompt a user for a number and save it
+second_n = input("second number:")
+sum =float(first_n) / float(second_n)
+print(f"{first_n} / {second_n} = {sum:.2f}")
+
+#exponent
+print("Enter two numbers to exponent:")
+#prompt a user for a number and save it
+first_n = input("first number:")
+#prompt a user for a number and save it
+second_n = input("second number:")
+sum =float(first_n) ** float(second_n)
+print(f"{first_n} ** {second_n} = {sum:.2f}")
 
