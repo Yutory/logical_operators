7588f9058244a1980b07be3e01a6fe7d033242a3
Yutory <bhareyutory@gmail.com>
2025-07-24 17:40:44 +0100

    uncommented few code parts

--- list.py
+++ list.py
@@ -1,7 +1,7 @@
-#code = ["X","H","E","Z","L","L","!","P","O","-","N"," ","W","@","R","D","O","#"]
-#Hello = code[1] + code[2] + code[4] + code[5] + code[8] + code[6]
-#print(Hello) 
-'''grid = [ 
+code = ["X","H","E","Z","L","L","!","P","O","-","N"," ","W","@","R","D","O","#"]
+Hello = code[1] + code[2] + code[4] + code[5] + code[8] + code[6]
+print(Hello) 
+grid = [ 
 ["The", "sky","is"],
 ["full","of","stars"],
 ["shining","bright","tonight"]
@@ -15,15 +15,15 @@ grid[2][0]
 )
 print(grid[2][-1],
 grid[2][-2],
-grid[2][-3])'''
+grid[2][-3])
 
-'''playlist = ["song A", "song B", "song C"]
+playlist = ["song A", "song B", "song C"]
 playlist[1] = "song D"
 print(playlist)
 playlist.append("song E")
 print(playlist)
 playlist.insert(0, "intro")
-print(playlist)'''
+print(playlist)
 
 desks = []
 desks.append(input("student name1:"))
@@ -31,9 +31,10 @@ desks.append(input("student name2:"))
 desks.append(input("student name3:"))
 print(desks)
 new_student = input("new_student name:")
-desks[1] = "new_student"
+desks[1] = new_student
 print(desks)
-desks.insert(1, "new_guy")
+new_guy = input("enter new_guy name:")
+desks.insert(1, new_guy)
 print(desks)
 
 
